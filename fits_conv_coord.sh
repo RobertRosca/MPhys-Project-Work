5610a5cc16143c6ce51cc265c07b16612c522d8d
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2017-10-21 16:53:46 +0100

    Added file checks
    If no files are found then the script exits, doesn't create `./readable` folder anymore.

--- fits_conv_coord.sh
+++ fits_conv_coord.sh
@@ -1,15 +1,18 @@
-echo 'Starting heasoft'
-source ~/Documents/heasoft-6.22.1/x86_64-unknown-linux-gnu-libc2.23/headas-init.sh
+files=$(ls | grep .evt)
 
-echo 'Found files:'
-files=$(ls | grep evt)
-echo $files
+if [ "$files" != "" ]; then
+	echo 'Starting heasoft'
+	source /home/robert/Documents/heasoft-6.22.1/x86_64-unknown-linux-gnu-libc2.23/headas-init.sh
 
-mkdir -p readable
+	echo 'Found files:'
+	echo $files
+	mkdir -p readable
+else
+	echo 'No ".evt" files found'
+fi
 
 for file in $files ; do
 	file_out=`basename $file .evt`.txt
-	#echo 'Converting TIME, X, Y, DET1X, DET1Y, DET2X, DET2Y for' $file
 	fdump infile=./$file[1] outfile=./readable/$file_out columns="TIME X Y DET1X DET1Y DET2X DET2Y" rows=-
 	echo 'Saved as' ./readable/$file_out
 done
